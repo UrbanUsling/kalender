9cda9b5884350006ff394c3617bd21f1fca8f81a
UrbanUsling <philip_akca@hotmail.com>
2025-05-01 01:09:40 +0200

    back

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: 'dist', // Vercel will serve from here
-  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
